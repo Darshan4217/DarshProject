c517198a2b9bf181af282e556c8ee6d31a693441
lenovo <manatri.darshan10@gmail.com>
2019-01-19 11:38:09 +0530

    Code refactor

--- app/src/main/java/com/example/lenovo/myapplication/Country/CountryFragment.java
+++ app/src/main/java/com/example/lenovo/myapplication/Country/CountryFragment.java
@@ -1,29 +1,18 @@
 package com.example.lenovo.myapplication.Country;
 
-import android.content.*;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.LocalBroadcastManager;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.example.lenovo.myapplication.R;
 import com.example.lenovo.myapplication.base.BackButtonSupportFragment;
 import com.example.lenovo.myapplication.base.BaseFragment;
-import com.example.lenovo.myapplication.utils.Config;
-import com.example.lenovo.myapplication.utils.NotificationUtils;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,8 +24,6 @@ public class CountryFragment extends BaseFragment implements BackButtonSupportFr
     FrameLayout countryListLayout;
     private Toast toast;
     private boolean consumingBackPress = true;
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-    private TextView txtRegId, txtMessage;
 
     public static CountryFragment newInstance() {
         return new CountryFragment();
@@ -46,10 +33,8 @@ public class CountryFragment extends BaseFragment implements BackButtonSupportFr
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_country_list, container, false);
+        view = inflater.inflate(R.layout.fragment_country_list, container, false);
         btnDetail = view.findViewById(R.id.btnDetail);
-        txtRegId = view.findViewById(R.id.txt_reg_id);
-        txtMessage = view.findViewById(R.id.txt_push_message);
 
         btnDetail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,45 +43,7 @@ public class CountryFragment extends BaseFragment implements BackButtonSupportFr
             }
         });
 
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-                // checking for type intent filter
-                if (intent.getAction().equals(Config.REGISTRATION_COMPLETE)) {
-                    // gcm successfully registered
-                    // now subscribe to `global` topic to receive app wide notifications
-                    FirebaseMessaging.getInstance().subscribeToTopic(Config.TOPIC_GLOBAL);
-
-                    displayFirebaseRegId();
-
-                } else if (intent.getAction().equals(Config.PUSH_NOTIFICATION)) {
-                    // new push notification is received
-
-                    String message = intent.getStringExtra("message");
-
-                    Toast.makeText(context.getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
-
-                    txtMessage.setText(message);
-                }
-            }
-        };
-
-        displayFirebaseRegId();
         return view;
-    }
-    // Fetches reg id from shared preferences
-    // and displays on the screen
-    private void displayFirebaseRegId() {
-        SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
-        String regId = pref.getString("regId", null);
-
-        Log.e(TAG, "Firebase reg id: " + regId);
-
-        if (!TextUtils.isEmpty(regId))
-            txtRegId.setText("Firebase Reg Id: " + regId);
-        else
-            txtRegId.setText("Firebase Reg Id is not received yet!");
     }
 
     @Override
@@ -124,29 +71,6 @@ public class CountryFragment extends BaseFragment implements BackButtonSupportFr
         toast.cancel();
         return false; //delegated
 
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // register GCM registration complete receiver
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(Config.REGISTRATION_COMPLETE));
-
-        // register new push message receiver
-        // by doing this, the activity will be notified each time a new message arrives
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(Config.PUSH_NOTIFICATION));
-
-        // clear the notification area when the app is opened
-        NotificationUtils.clearNotifications(getActivity().getApplicationContext());
-    }
-
-    @Override
-    public void onPause() {
-        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
     }
 
 }
